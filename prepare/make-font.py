7ce1817f059b2f90ac5723cbf9e45c8c0fce39e9
osfans <waxaca@163.com>
2020-10-28 09:17:16 +0800

    intro

--- prepare/make-font.py
+++ prepare/make-font.py
@@ -3,15 +3,17 @@
 import fontforge
 
 hz = ""
-for line in open("han.txt"):
+for line in open("缺字"):
     hz+=line.strip()
-us = list(map(ord,hz))
-f="han.ttf"
-font = fontforge.open(f)
+missing = set(map(ord,hz))
+
+font = fontforge.open("98WB-U.ttf")
+has = set()
 for g in font.glyphs():
     u = g.unicode
-    if u >= 0x40000 or u not in us:
+    has.add(u)
+    if u not in missing:
         font.removeGlyph(g)
 font.generate("../app/src/main/res/font/han.ttf")
 font.close()
-
+print("".join(map(chr,missing-has)))
---
+++ prepare/yt.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+from openpyxl import load_workbook
+from collections import defaultdict
+import os.path
+
+def get_dict():
+	yt = dict()
+	if os.path.exists("yt.txt"):
+		f = open("yt.txt")
+		for line in f:
+			fs = line.strip().split(" ")
+			yt[int(fs[0])] = fs[1]
+		f.close()
+	else:
+		for sheet in load_workbook("韵图音系同音字表.xlsx"):
+			for row in sheet.rows:
+				y = row[0].value
+				for cell in row[1:]:
+					if v := cell.value:
+						if type(v) is float:
+							yt[int(v)]=y
+						elif type(v) is str and "#" in v:
+							for i in v.split("#"):
+								yt[int(i)]=y
+		f = open("yt.txt", "w")
+		for i in sorted(yt.keys()):
+			print(i,yt[i],file=f)
+		f.close()
+	
+	pq = dict()
+	for line in open("../../ytenx/ytenx/sync/kyonh/PrengQim.txt"):
+		line = line.strip()
+		if line.startswith("#"): continue
+		fs = line.split(" ")
+		pq[fs[1]] = yt[int(fs[0])]
+
+	d=defaultdict(list)
+	for line in open("../../RIME/rime-middle-chinese/zyenpheng.dict.yaml"):
+		line = line.strip()
+		fs = line.split('\t')
+		if len(fs) < 2: continue
+		hz, py = fs[:2]
+		if len(hz) == 1 and py in pq:
+			d[hz].append(pq[py])
+	return d
