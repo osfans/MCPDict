9b0efe4e57e1934a66de187e43061e59f24d6933
osfans <waxaca@163.com>
2020-10-28 09:17:16 +0800

    intro

--- prepare/generate.py
+++ prepare/generate.py
@@ -4,6 +4,10 @@ import sqlite3, re, json
 from collections import defaultdict
 import logging
 from time import time
+import ruamel.yaml
+from itertools import chain
+import unicodedata
+from openpyxl import load_workbook
 
 logging.basicConfig(format='[%(asctime)s] %(message)s', level=logging.INFO)
 start = time()
@@ -21,33 +25,37 @@ def hex2chr(uni):
     return chr(int(uni, 16))
 
 HEADS = [
-  ('hz', '漢字', '漢字', '#9D261D', '字海', 'http://yedict.com/zscontent.asp?uni=%2$s'),
+  ('hz', '漢字', '漢字', '#9D261D', '字海', 'http://yedict.com/zscontent.asp?uni=%2$s',"本程序可以查詢漢字在古今中外多種語言中的讀音，可輸入普通話拼音、漢字、Unicode編碼、筆畫、部首進行查詢。"),
   #('unicode', '統一碼', '統一碼', '#808080', 'Unihan', 'https://www.unicode.org/cgi-bin/GetUnihanData.pl?codepoint=%s'),
-  ('bh', '總筆畫數', '筆畫', '#808080', None, None),
-  ('bs', '部首餘筆', '部首', '#808080', None, None),
-  ('sg', '上古', '上古', '#9A339F', '韻典網（上古音系）', 'https://ytenx.org/dciangx/dzih/%s'),
-  ('mc', '中古', '中古', '#9A339F', '韻典網', "http://ytenx.org/zim?kyonh=1&dzih=%s"),
-  ('zy', '中原音韻', '近古', '#9A339F', '韻典網（中原音韻）', 'https://ytenx.org/trngyan/dzih/%s'),
-  ('pu', '普通話', '普語', '#FF00FF', '漢典網', "http://www.zdic.net/hans/%s"),
-  ('nt', '南通話', '南通', '#0000FF', '南通方言網', "http://nantonghua.net/search/index.php?hanzi=%s"),
-  ('tr', '泰如方言', '泰如', '#0000FF', '泰如小字典', "http://taerv.nguyoeh.com/"),
-  ('ic', '鹽城話', '鹽城', '#0000FF', '淮語字典', "https://huae.sourceforge.io/query.php?table=類音字彙&字=%s"),
+  ('bh', '總筆畫數', '筆畫', '#808080', None, None, None),
+  ('bs', '部首餘筆', '部首', '#808080', None, None, None),
+  ('sg', '上古（鄭張尚芳）', '上古', '#9A339F', '韻典網（上古音系）', 'https://ytenx.org/dciangx/dzih/%s',"上古音來自韻典網的《<a href=https://ytenx.org/dciangx/>上古音系</a>》"),
+  ('ba', '上古（白一平沙加爾）', '白沙', '#9A339F', None, None, "上古音白一平沙加爾擬音來自：<a href=http://ocbaxtersagart.lsait.lsa.umich.edu/>http://ocbaxtersagart.lsait.lsa.umich.edu/</a>"),
+  ('mc', '中古', '中古', '#9A339F', '韻典網', "http://ytenx.org/zim?kyonh=1&dzih=%s", "中古音主要來自韻典網的《<a href=https://ytenx.org/kyonh/>廣韻</a>》，還有一部分讀音來自<a href=https://github.com/biopolyhedron/rime-middle-chinese>中古全拼輸入法</a>，以灰色顯示"),
+  ('yt', '韻圖', '韻圖', '#9A339F', None, None, "韻圖擬音來自QQ共享文檔<a href=https://docs.qq.com/sheet/DYk9aeldWYXpLZENj>韻圖音系同音字表</a>"),
+  ('zy', '中原音韻', '近古', '#9A339F', '韻典網（中原音韻）', 'https://ytenx.org/trngyan/dzih/%s', "近古音來自韻典網的《<a href=https://ytenx.org/trngyan/>中原音韻</a>》"),
+  ('pu', '普通話', '普語', '#FF00FF', '漢典網', "http://www.zdic.net/hans/%s", "普通話讀音主要來自<a href=https://www.zdic.net/>漢典</a>與<a href=http://yedict.com/>字海</a>，還有一部分讀音來自<a href=https://www.moedict.tw/>萌典</a>，以灰色顯示"),
+  ('nt', '南通話', '南通', '#0000FF', '南通方言網', "http://nantonghua.net/search/index.php?hanzi=%s", "南通話讀音來自<a href=http://nantonghua.net/archives/5127/南通话字音查询/>南通方言網</a>"),
+  ('tr', '泰如方言', '泰如', '#0000FF', '泰如小字典', "http://taerv.nguyoeh.com/query.php?table=泰如字典&簡體=%s", "泰如方言讀音來自<a href=http://taerv.nguyoeh.com/>泰如小字典</a>"),
+  ('ic', '鹽城話', '鹽城', '#0000FF', '淮語字典', "https://huae.sourceforge.io/query.php?table=類音字彙&字=%s", "鹽城話讀音主要來自<a href=http://huae.nguyoeh.com/>類音字彙</a>，並補充了《鹽城縣志》等讀音"),
   #('lj', '南京話', '南京', '#0000FF', '南京官話拼音方案', "https://uliloewi.github.io/LangJinPinIn/PinInFangAng"),
-  ('sz', '蘇州話', '蘇州', '#1E90FF', '吳語學堂（蘇州）', "https://www.wugniu.com/search?table=suzhou_zi&char=%s"),
-  ('sh', '上海話', '上海', '#1E90FF', '吳音小字典（上海）', "http://www.wu-chinese.com/minidict/search.php?searchlang=zaonhe&searchkey=%s"),
-  ('nc', '南昌話', '南昌', '#00ADAD', None, None),
-  ('hk', '客家話綜合口音', '客語', '#008000', '薪典', "https://www.syndict.com/w2p.php?item=hak&word=%s"),
-  ('hl', '客家話海陸腔', '海陸', '#008000', '客語萌典', "https://www.moedict.tw/:%s"),
-  ('sx', '客家話四縣腔', '四縣', '#008000', '客語萌典', "https://www.moedict.tw/:%s"),
-  ('ct', '粵語', '粵語', '#FFAD00', '粵語審音配詞字庫', "http://humanum.arts.cuhk.edu.hk/Lexis/lexi-can/search.php?q=%3$s"),
-  ('mn', '閩南語', '閩南', '#FF6600', '臺灣閩南語常用詞辭典', "http://twblg.dict.edu.tw/holodict_new/result.jsp?querytarget=1&radiobutton=0&limit=20&sample=%s"),
-  ('vn', '越南語', '越南', '#DB7093', '漢越辭典摘引', "http://www.vanlangsj.org/hanviet/hv_timchu.php?unichar=%s"),
-  ('kr', '朝鮮語', '朝鮮', '#BA55D3', 'Naver漢字辭典', "http://hanja.naver.com/hanja?q=%s"),
-  ('jp_go', '日語吳音', '日吳', '#FF0000', None, None),
-  ('jp_kan', '日語漢音', '日漢', '#FF0000', None, None),
-  ('jp_tou', '日語唐音', '日唐', '#FF0000', None, None),
-  ('jp_kwan', '日語慣用音', '日慣', '#FF0000', None, None),
-  ('jp_other', '日語其他讀音', '日他', '#FF0000', None, None),
+  ('sz', '蘇州話', '蘇州', '#1E90FF', '吳語學堂（蘇州）', "https://www.wugniu.com/search?table=suzhou_zi&char=%s", "蘇州話讀音來自<a href=https://www.wugniu.com/>吳語學堂</a>"),
+  ('sh', '上海話', '上海', '#1E90FF', '吳音小字典（上海）', "http://www.wu-chinese.com/minidict/search.php?searchlang=zaonhe&searchkey=%s", "上海話讀音來自《上海市區方言志》（1988年版），感謝蔡子文錄入。該書記錄的是中派上海話音系（使用者多出生於20世紀40至70年代），與<a href=http://www.wu-chinese.com/minidict/>吳音小字典</a>記錄的音系並不完全相同。"),
+  ('ra', '瑞安城關', '瑞安', '#1E90FF', None, None, "瑞安城關讀音由<u>落橙</u>提供"),
+  ('nc', '南昌話', '南昌', '#00ADAD', None, None, "南昌話讀音由<u>澀口的茶</u>提供"),
+  ('hk', '客家話綜合口音', '客語', '#008000', '薪典', "https://www.syndict.com/w2p.php?item=hak&word=%s", "客家話綜合口音來自<a href=https://www.syndict.com/>薪典</a>"),
+  ('hl', '客家話海陸腔', '海陸', '#008000', '客語萌典', "https://www.moedict.tw/:%s", "客家話海陸腔讀音來自<a href=https://www.moedict.tw/>客語萌典</a>"),
+  ('sx', '客家話四縣腔', '四縣', '#008000', '客語萌典', "https://www.moedict.tw/:%s", "客家話四縣腔讀音來自<a href=https://www.moedict.tw/>客語萌典</a>"),
+  ('ct', '粵語', '粵語', '#FFAD00', '粵語審音配詞字庫', "http://humanum.arts.cuhk.edu.hk/Lexis/lexi-can/search.php?q=%3$s", "粵語讀音主要來自<a href=http://humanum.arts.cuhk.edu.hk/Lexis/lexi-can/>粵語審音配詞字庫</a>，還補充了<a href=http://www.unicode.org/charts/unihan.html>Unihan</a>的部分讀音"),
+  ('mn', '閩南語', '閩南', '#FF6600', '臺灣閩南語常用詞辭典', "http://twblg.dict.edu.tw/holodict_new/result.jsp?querytarget=1&radiobutton=0&limit=20&sample=%s", "閩南語讀音來自<a href=https://twblg.dict.edu.tw/holodict_new/>臺灣閩南語常用詞辭典</a>"),
+  ('vn', '越南語', '越南', '#DB7093', '漢越辭典摘引', "http://www.vanlangsj.org/hanviet/hv_timchu.php?unichar=%s", "越南語讀音來自<a href=http://www.vanlangsj.org/hanviet/>漢越辭典摘引</a>"),
+  ('km', '中世紀朝鮮語', '朝中', '#BA55D3', None, None, "中世紀朝鮮語讀音來自<a href=https://github.com/nk2028/sino-korean-readings>韓國漢字音歷史層次研究</a>"),
+  ('kr', '朝鮮語', '朝鮮', '#BA55D3', 'Naver漢字辭典', "http://hanja.naver.com/hanja?q=%s", "朝鮮語讀音來自<a href=http://hanja.naver.com/>Naver漢字辭典</a>"),
+  ('jp_go', '日語吳音', '日吳', '#FF0000', None, None, "日語讀音來自《漢字源》改訂第五版。《漢字源》區分了漢字的吳音、漢音、唐音與慣用音，並提供了“歷史假名遣”寫法。該辭典曾經有<a href=http://ocn.study.goo.ne.jp/online/contents/kanjigen/>在線版本</a>，但已於2014年1月底終止服務。"),
+  ('jp_kan', '日語漢音', '日漢', '#FF0000', None, None, "日語讀音來自《漢字源》改訂第五版。《漢字源》區分了漢字的吳音、漢音、唐音與慣用音，並提供了“歷史假名遣”寫法。該辭典曾經有<a href=http://ocn.study.goo.ne.jp/online/contents/kanjigen/>在線版本</a>，但已於2014年1月底終止服務。"),
+  ('jp_tou', '日語唐音', '日唐', '#FF0000', None, None, "日語讀音來自《漢字源》改訂第五版。《漢字源》區分了漢字的吳音、漢音、唐音與慣用音，並提供了“歷史假名遣”寫法。該辭典曾經有<a href=http://ocn.study.goo.ne.jp/online/contents/kanjigen/>在線版本</a>，但已於2014年1月底終止服務。"),
+  ('jp_kwan', '日語慣用音', '日慣', '#FF0000', None, None, None),
+  ('jp_other', '日語其他讀音', '日他', '#FF0000', None, None, None),
 ]
 ZHEADS = list(zip(*HEADS))
 KEYS = ZHEADS[0]
@@ -66,9 +74,6 @@ for r in c.execute('SELECT * FROM mcpdict'):
   row["hz"] = i
   unicodes[i] = row
 conn.close()
-
-# patch
-unicodes["冇"]["kr"]=None
 logging.info("讀取數據庫 %.3f" % timeit())
 
 kCompatibilityVariants = dict()
@@ -95,9 +100,8 @@ for line in open("/usr/share/unicode/Unihan_IRGSources.txt"):
 logging.info("處理兼容字 %.3f" % timeit())
 
 #mc
-#https://github.com/biopolyhedron/rime-middle-chinese/blob/master/zyenpheng.dict.yaml
 d.clear()
-for line in open("zyenpheng.dict.yaml"):
+for line in open("../../RIME/rime-middle-chinese/zyenpheng.dict.yaml"):
   line = line.strip()
   fs = line.split('\t')
   if len(fs) < 2: continue
@@ -131,6 +135,13 @@ for hz in unicodes.keys():
         unicodes[hz]["mc"] = "|%s|"%(py.replace(",", "|,|"))
 logging.info("處理中古音 %.3f" % timeit())
 
+#yt
+import yt
+d.clear()
+d = yt.get_dict()
+update("yt", d)
+logging.info("處理韻圖 %.3f" % timeit())
+
 #sg
 #https://github.com/BYVoid/ytenx/blob/master/ytenx/sync/dciangx/DrienghTriang.txt
 d.clear()
@@ -145,6 +156,20 @@ for line in open("../../ytenx/ytenx/sync/dciangx/DrienghTriang.txt"):
       d[hz].append(py)
 update("sg", d)
 logging.info("處理上古音 %.3f" % timeit())
+
+#ba
+#http://ocbaxtersagart.lsait.lsa.umich.edu/BaxterSagartOC2015-10-13.xlsx
+d.clear()
+for sheet in load_workbook("BaxterSagartOC2015-10-13.xlsx"):
+  for row in sheet.rows:
+    if row:
+      hz = row[0].value
+      py = row[4].value
+      if len(hz) == 1:
+        if py not in d[hz]:
+          d[hz].append(py)
+update("ba", d)
+logging.info("處理白沙2015 %.3f" % timeit())
 
 #zy
 #https://github.com/BYVoid/ytenx/blob/master/ytenx/sync/trngyan
@@ -235,13 +260,13 @@ logging.info("處理泰如話 %.3f" % timeit())
 #https://github.com/osfans/xu/blob/master/docs/xu.csv
 d.clear()
 icsm = {'g': 'k', 'd': 't', '': '', 'c': 'tsʰ', 'b': 'p', 'l': 'l', 'h': 'x', 't': 'tʰ', 'q': 'tɕʰ', 'z': 'ts', 'j': 'tɕ', 'f': 'f', 'k': 'kʰ', 'n': 'n', 'x': 'ɕ', 'm': 'm', 's': 's', 'p': 'pʰ', 'ng': 'ŋ'}
-icym = {'ae': 'ɛ', 'ieh': 'iəʔ', 'ii': 'i', 'eh': 'əʔ', 'io': 'iɔ', 'ieu': 'iəu', 'u': 'u', 'v': 'v', 'en': 'ən', 'a': 'a', 'on': 'ɔŋ', 'an': 'ã', 'oh': 'ɔʔ', 'i': 'j', 'ien': 'in', 'ion': 'iɔŋ', 'ah': 'aʔ', 'ih': 'iʔ', 'y': 'y', 'ui': 'ui', 'uae': 'uɛ', 'aeh': 'ɛʔ', 'in': 'ĩ', 'ia': 'ia', 'z': 'ɿ', 'uh': 'uʔ', 'aen': 'ɛ̃', 'er': 'ɚ', 'eu': 'əu', 'iah': 'iaʔ', 'ueh': 'uəʔ', 'iae': 'iɛ', 'iuh': 'iuʔ', 'yen': 'yn', 'ian': 'iã', 'iun': 'iũ', 'un': 'ũ', 'o': 'ɔ', 'uan': 'uã', 'ua': 'ua', 'uen': 'uən', 'ioh': 'iɔʔ', 'iaen': 'iɛ̃', 'uaen': 'uɛ̃', 'uaeh': 'uɛʔ', 'iaeh': 'iɛʔ', 'uah': 'uaʔ', 'yeh': 'yəʔ', 'ya': 'ya'}
+icym = {'ae': 'ɛ', 'ieh': 'iəʔ', 'ii': 'i', 'eh': 'əʔ', 'io': 'iɔ', 'ieu': 'iəu', 'u': 'u', 'v': 'v', 'en': 'ən', 'a': 'a', 'on': 'ɔŋ', 'an': 'ã', 'oh': 'ɔʔ', 'i': 'j', 'ien': 'in', 'ion': 'iɔŋ', 'ah': 'aʔ', 'ih': 'iʔ', 'y': 'y', 'ui': 'ui', 'uae': 'uɛ', 'aeh': 'ɛʔ', 'in': 'ĩ', 'ia': 'ia', 'z': 'ɿ', 'uh': 'uʔ', 'aen': 'ɛ̃', 'er': 'ɚ', 'eu': 'əu', 'iah': 'iaʔ', 'ueh': 'uəʔ', 'iae': 'iɛ', 'iuh': 'iuʔ', 'yen': 'yn', 'ian': 'iã', 'iun': 'iũ', 'un': 'ũ', 'o': 'ɔ', 'uan': 'uã', 'ua': 'ua', 'uen': 'uən', 'ioh': 'iɔʔ', 'iaen': 'iɛ̃', 'uaen': 'uɛ̃', 'uaeh': 'uɛʔ', 'iaeh': 'iɛʔ', 'uah': 'uaʔ', 'yeh': 'yəʔ', 'ya': 'ya', '': ''}
 for line in open("ic"):
   line = line.strip()
   if not line: continue
   fs = line.split('\t')
   py,hzs = fs
-  sm = re.findall("^[^aeiouvy]?", py)[0]
+  sm = re.findall("^[^aeiouvy]?g?", py)[0]
   sd = py[-1]
   ym = py[len(sm):-1]
   py = icsm[sm]+icym[ym]+sd
@@ -325,14 +350,14 @@ logging.info("處理蘇州話 %.3f" % timeit())
 #pu
 def norm(py):
     if py == "wòng": py= "weng4"
+    py = re.sub('(\w)([klwtf])', '\\1 \\2', py)
     py = py.replace('ɑ', 'a').replace('ɡ', 'g').replace('ü','v')
     tonea=['ā', 'á', 'ǎ', 'à', 'ē', 'é', 'ě', 'è', 'ī', 'í', 'ǐ', 'ì', 'ō', 'ó', 'ǒ', 'ò', 'ū', 'ú', 'ǔ', 'ù', 'ǘ', 'ǚ', 'ǜ', 'ń', 'ň', 'ǹ', 'm̄', 'ḿ', 'm̀','ê̄','ế','ê̌','ề']
     toneb=["a1","a2","a3","a4","e1","e2","e3","e4","i1","i2","i3","i4","o1","o2","o3","o4","u1","u2","u3","u4","v2","v3","v4","n2","n3","n4","m1","m2", "m4",'ea1','ea2','ea3','ea4']
     for i in tonea:
       if i in py:
         py=py.replace(i, toneb[tonea.index(i)])
-        break
-    py=re.sub("(\d)(.*)$", r'\2\1', py)
+    py=re.sub("(\d)(.*?)\\b", r'\2\1', py)
     return py
 
 d.clear()
@@ -347,13 +372,22 @@ for line in open("/usr/share/unicode/Unihan_Readings.txt"):
         d[han].append(norm(y))
 update("pu", d)
 logging.info("處理普通話 %.3f" % timeit())
+for line in open("zdic.txt"):
+    if not line.startswith("U"): continue
+    fields = line.strip().split(" ")
+    han = fields[4]
+    yin = fields[1]
+    for y in yin.split(","):
+      d[han].append(norm(y))
+update("pu", d)
+logging.info("處理漢典音 %.3f" % timeit())
 
 #https://github.com/g0v/moedict-data-csld/blob/master/中華語文大辭典全稿-20160620.csv
 def update_twpy(hz, py):
   if len(hz) != 1 or len(py) == 0: return
   k = "pu"
   v = unicodes[hz].get(k, None)
-  pyu = "_%s_" % py
+  pyu = "|%s|" % py
   if v:
     vl = v.split(",")
     if py not in vl and pyu not in vl:
@@ -442,6 +476,27 @@ for i in unicodes.keys():
       unicodes[i]["sh"] = sh
 logging.info("處理上海話 %.3f" % timeit())
 
+#ra
+def ra2ipa(s):
+  s = re.sub("([^aeiouy])u([1-8])", "\\1ʋʷ\\2", s)
+  s = s.replace("y", "J").replace("io","yo").replace("Jo","yo").replace("ae", "ɛ").replace("ao", "ɔ").replace("eu", "əʉ").replace("ou", "əu").replace("oe", "ø").replace("yu", "y").replace("iu", "y")\
+          .replace("an", "aŋ").replace("en", "əŋ").replace("on", "oŋ")
+  s = s.replace("ph", "pʰ").replace("th", "tʰ").replace("kh", "kʰ").replace("tsh", "tsʰ")\
+          .replace("ch", "tɕʰ").replace("c", "tɕ").replace("sh", "ɕ").replace("j", "dʑ").replace("J","j").replace("zh", "ʑ")\
+          .replace("gh", "ɦ").replace("ng", "ŋ")
+  return s
+d.clear()
+for line in open("同音字表-瑞安.csv"):
+  fs = line.strip().split('\t')
+  hz = fs[0]
+  py = fs[1]
+  if len(hz) == 1:
+    py = ra2ipa(py)
+    if py not in d[hz]:
+      d[hz].append(py)
+update("ra", d)
+logging.info("處理瑞安話 %.3f" % timeit())
+
 #mn
 for i in unicodes.keys():
   if "mn" in unicodes[i]:
@@ -465,7 +520,7 @@ def hk2ipa(s, tones):
     s = s[:-1]
   else:
     c = ""
-  s = s.replace("er","ə").replace("ae","æ").replace("ii", "ɿ").replace("e", "ɛ")
+  s = s.replace("er","ə").replace("ae","æ").replace("ii", "ɿ").replace("e", "ɛ").replace("o", "ɔ")
   s = s.replace("sl", "ɬ").replace("nj", "ɲ").replace("t", "tʰ").replace("zh", "t∫").replace("ch", "t∫ʰ").replace("sh", "∫").replace("p", "pʰ").replace("k", "kʰ").replace("z", "ts").replace("c", "tsʰ").replace("j", "tɕ").replace("q", "tɕʰ").replace("x", "ɕ").replace("rh", "ʒ").replace("r", "ʒ").replace("ng", "ŋ").replace("?", "ʔ").replace("b", "p").replace("d", "t").replace("g", "k")
   tone = re.findall("[¹²³⁴⁵\d]+$", s)
   if tone:
@@ -518,36 +573,59 @@ logging.info("處理客家話 %.3f" % timeit())
 readings = "白文又"
 d.clear()
 def get_readings(index):
-	if not index: return ''
-	return "`%s`"%readings[int(index)-1]
+  if not index: return ''
+  return "`%s`"%readings[int(index)-1]
 def readorder(py):
-	index = readings.index(py[-2])+1 if py.endswith('`') else 0
-	return "%d%s"%(index,py)
+  index = readings.index(py[-2])+1 if py.endswith('`') else 0
+  return "%d%s"%(index,py)
 def sub(m):
-	ret = ''
-	for i in m.group(1):
-		if i in "12345":continue
-		ret += i + m.group(2)
-	return ret
+  ret = ''
+  for i in m.group(1):
+    if i in "12345":continue
+    ret += i + m.group(2)
+  return ret
 for line in open("nc"):
-	line = line.strip().replace(" ", "").replace("(", "（").replace(")","）")
-	if line.startswith("#") or not line: continue
-	line = re.sub('（(.*?)）(\d)', sub, line)
-	fs = re.findall("^([^\u3400-\U0003134f]+ ?)(.*)$", line)[0]
-	py, hzs = fs
-	if not hzs: continue
-	for hz,r in re.findall("(.)(\d?)", hzs):
-		item = py+get_readings(r)
-		if item not in d[hz]:
-			d[hz].append(item)
-		if py.endswith('k7') or py.endswith('k8'):
-			item = re.sub('k([78])', 'ʔ\\1', py)+get_readings('3')
-			if item not in d[hz]:
-				d[hz].append(item)
+  line = line.strip().replace(" ", "").replace("(", "（").replace(")","）")
+  if line.startswith("#") or not line: continue
+  line = re.sub('（(.*?)）(\d)', sub, line)
+  fs = re.findall("^([^\u3400-\U0003134f]+ ?)(.*)$", line)[0]
+  py, hzs = fs
+  if not hzs: continue
+  for hz,r in re.findall("(.)(\d?)", hzs):
+    item = py+get_readings(r)
+    if item not in d[hz]:
+      d[hz].append(item)
+    if py.endswith('k7') or py.endswith('k8'):
+      item = re.sub('k([78])', 'ʔ\\1', py)+get_readings('3')
+      if item not in d[hz]:
+        d[hz].append(item)
 for hz in d:
-	d[hz] = sorted(d[hz], key=readorder)
+  d[hz] = sorted(d[hz], key=readorder)
 update("nc", d)
 logging.info("處理南昌話 %.3f" % timeit())
+
+#km
+#https://github.com/nk2028/sino-korean-readings/blob/main/woosun-sin.csv
+d.clear()
+for line in open("woosun-sin.csv"):
+  fs = line.strip().split(',')
+  hz = fs[0]
+  if len(hz) == 1:
+    py = "".join(fs[1:4])
+    if py not in d[hz]:
+      d[hz].append(py)
+update("km", d)
+logging.info("處理中世朝 %.3f" % timeit())
+
+#patch
+patch = ruamel.yaml.load(open("patch.yaml"), Loader=ruamel.yaml.Loader)
+for lang in patch:
+  for hz in patch[lang]:
+    for i in hz:
+      if i not in unicodes:
+        unicodes[i]["hz"] = i
+      unicodes[i][lang] = patch[lang][hz]
+logging.info("修正汉字音 %.3f" % timeit())
 
 #bh
 d.clear()
@@ -598,17 +676,38 @@ c.execute("DROP TABLE IF EXISTS mcpdict")
 c.execute("CREATE VIRTUAL TABLE mcpdict USING fts3 (%s)"%FIELDS)
 c.executemany(INSERT, ZHEADS[1:])
 
-f = open("han.txt","w")
+f = open("缺字","w")
+fpy = open("缺音", "w")
 for i in sorted(unicodes.keys(), key=cjkorder):
   n = ord(i)
   if 0xE000<=n<=0xF8FF or 0xF0000<=n<=0xFFFFD or 0x100000<=n<=0x10FFFD:
     continue
+  if (n<0x3400 and n!=0x3007) or n>0x3134F:
+    continue
   d = unicodes[i]
   v = list(map(d.get, KEYS))
   c.execute(INSERT, v)
-  if n >= 0x20000:
+  if n >= 0x20000 or 0x9FD1<=n<=0x9FFF or 0x4DB6<=n<=0x4DBF:
     f.write(i)
+  if not d.get("pu"):
+    fpy.write(i)
 f.close()
+
+for i in chain(range(0x3400,0xa000),range(0x20000,0x31350)):
+  c = chr(i)
+  if c in unicodes:
+    d = unicodes[c]
+    if not d.get("pu"):
+      fpy.write(c)
+  else:
+    try:
+      name = unicodedata.name(c)
+      if name.startswith("CJK UNIFIED"):
+        fpy.write(c)
+    except:
+      pass
+fpy.close()
+
 conn.commit()
 conn.close()
 logging.info("保存數據庫 %.3f" % timeit())
