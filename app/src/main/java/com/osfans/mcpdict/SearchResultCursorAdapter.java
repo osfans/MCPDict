cccceb4ee5d8c6566cb0c3c115db56f2f6056e97
osfans <waxaca@163.com>
2021-06-23 16:49:12 +0800

    refactor: Displayer

--- app/src/main/java/com/osfans/mcpdict/SearchResultCursorAdapter.java
+++ app/src/main/java/com/osfans/mcpdict/SearchResultCursorAdapter.java
@@ -250,48 +250,6 @@ public class SearchResultCursorAdapter extends CursorAdapter {
         return s.replaceAll("[~_|*\\[\\]]", "").replaceAll("`.+?`", "");
     }
 
-    private static boolean isIPA(char c) {
-        int type = Character.getType(c);
-        return Character.isLetterOrDigit(c)
-                || type == Character.NON_SPACING_MARK
-                || type == Character.OTHER_NUMBER;
-    }
-
-    private abstract static class Displayer {
-        protected static final String NULL_STRING = "-";
-
-        public String display(String s) {
-            if (s == null) return NULL_STRING;
-            s = lineBreak(s);
-            // Find all regions of [a-z0-9]+ in s, and apply displayer to each of them
-            StringBuilder sb = new StringBuilder();
-            int L = s.length(), p = 0;
-            while (p < L) {
-                int q = p;
-                while (q < L && isIPA(s.charAt(q))) q++;
-                if (q > p) {
-                    String t1 = s.substring(p, q);
-                    String t2 = displayOne(t1);
-                    sb.append(t2 == null ? t1 : t2);
-                    p = q;
-                }
-                while (p < L && !isIPA(s.charAt(p))) p++; //
-                sb.append(s.substring(q, p));
-            }
-            // Add spaces as hints for line wrapping
-            s = sb.toString().replace(",", ", ")
-                             .replace("(", " (")
-                             .replace("]", "] ")
-                             .replaceAll(" +", " ")
-                             .replace(" ,", ",")
-                             .trim();
-            return s;
-        }
-
-        public String lineBreak(String s) {return s;}
-        public abstract String displayOne(String s);
-    }
-
     private final Displayer middleChineseDisplayer = new Displayer() {
         public String lineBreak(String s) {return s.replace(",", "\n");}
         public String displayOne(String s) {return Orthography.MiddleChinese.display(s, getStyle(R.string.pref_key_mc_display)) + middleChineseDetailDisplayer.display(s);}
