4a14191976aa2c1a708285cb0d5ca0b4de4a6fbe
osfans <waxaca@163.com>
2021-09-15 18:07:20 +0800

    unicode 14

--- prepare/make-font.py
+++ prepare/make-font.py
@@ -4,7 +4,7 @@ import fontforge
 
 notoext = set(map(ord,open("NotoSansCJK-Regular.txt").read().strip()))
 
-font = fontforge.open("TH-Feon.ttf")
+font = fontforge.open("TH-Feon.sfd")
 for g in font.glyphs():
     u = g.unicode
     if u in notoext or u < 0x3400 or u >= 0x31350 or 0xa000<= u < 0x20000:
