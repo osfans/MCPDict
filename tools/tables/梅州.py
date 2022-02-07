ba5344aea8461c1cc43cd2774168b4a5d93ee62c
osfans <waxaca@163.com>
2022-02-07 14:25:32 +0800

    0207

--- tools/tables/梅州.py
+++ tools/tables/梅州.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from tables._縣志 import 字表 as 表
+import re
 
 class 字表(表):
 	key = "hak_yt_mz"
@@ -11,9 +12,16 @@ class 字表(表):
 	simplified = 2
 
 	def format(self, line):
-		for i,j in self.toneValues.items():
-			line = line.replace("[%s]" % i, "[%s]" % j)
+		line = line.replace('"', '')
 		if "\t" in line:
 			fs = line.split("\t")
-			line = fs[0]+"\t"+("".join(fs[1:]))
+			line = fs[0]+"\t"
+			for i in fs[1:]:
+				if i.startswith("["):
+					results = re.findall("^\[(\d+)\](.+)$", i)
+					if results:
+						sd,hzs = results[0]
+						sd = "[%s]"%self.toneValues[sd]
+						i = sd + hzs
+				line += i
 		return line
