4622c7fd8de99aa0165bfa9978ed5ab36ec522a6
osfans <waxaca@163.com>
2022-03-04 17:14:19 +0800

    fix:嶽→岳

--- app/src/main/java/com/osfans/mcpdict/SearchResultCursorAdapter.java
+++ app/src/main/java/com/osfans/mcpdict/SearchResultCursorAdapter.java
@@ -138,7 +138,7 @@ public class SearchResultCursorAdapter extends CursorAdapter {
             if (width == 0) width = getMaxWidth(textViewName);
             formatTextView(textViewName, i);
             textViewName.setText(name);
-            if (name.length() != 2) textViewName.setTextScaleX(width/(float)getMeasuredWidth(textViewName));
+            if (name.length() > 2) textViewName.setTextScaleX(width/(float)getMeasuredWidth(textViewName));
             final TextView textViewDetail = row.findViewById(R.id.text_detail);
             textViewDetail.setTag(i);
             row.setTag("row" + i);
