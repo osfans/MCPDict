e1082ff6386908aee73039ab47b41fae7823a610
osfans <waxaca@163.com>
2022-03-11 18:56:45 +0800

    0311

---
+++ tools/tables/桐城南.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from tables._縣志 import 字表 as 表
+
+class 字表(表):
+	key = "cmn_jh_hx_aq_tcn"
+	note = "來源：“空島。”整理"
+	tones = "31 1 1a 陰平 ꜀,24 2 1b 陽平 ꜁,213 3 2 上 ꜂,,53 5 3a 陰去 ꜄,42 6 3b 陽去 ꜅,5 7 4 入 ꜆"
+	_file = "安徽省桐城南部方言同音字表*.tsv"
+	simplified = 2
