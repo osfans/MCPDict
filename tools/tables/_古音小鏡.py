add29419dd6623b0dc6801f29bfef91f9b803720
osfans <waxaca@163.com>
2022-05-12 16:37:56 +0800

    fix: 導入輕聲字

--- tools/tables/_古音小鏡.py
+++ tools/tables/_古音小鏡.py
@@ -8,23 +8,21 @@ class 表(_表):
 
 	def parse(self, fs):
 		if len(fs) < 4: return
-		if fs[2].isdigit():
+		name = str(self)
+		if name in ("塔玆語", "海倫","宜興南",):
 			hz, sy, tv, js = fs[:4]
-		elif fs[3].isdigit():
+		elif name in ("虎林", "吳江菀坪","景寧畬話","慈谿觀海衛",):
 			hz, sm, ym, tv, js = fs[:5]
 			sy = sm + ym
-		elif str(self) == "滁州":
+		elif name in ("滁州",):
 			_, hz, sm, ym, tv, js = fs[:6]
 			sy = sm + ym
-		elif fs[4].isdigit():
+		elif name in ("宣平",):
+			hz, _, sy, js = fs[:4]
+			sy, tv = self.splitSySd(sy)
+		else: #if name in ("淮南","懷遠","鳳陽","陽新新街",):
 			_, hz, sm, ym, tv, _, js = fs[:7]
 			sy = sm + ym
-		elif fs[2] and fs[2][-1].isdigit():
-			hz, _, sy, js = fs[:4]
-			tv = re.findall("\d+$", sy)[0]
-			sy = sy.rstrip("012345")
-		else:
-			return
 		if not hz or tv == "调": return
 		yb = self.dz2dl(sy, tv)
 		return hz, yb, js
