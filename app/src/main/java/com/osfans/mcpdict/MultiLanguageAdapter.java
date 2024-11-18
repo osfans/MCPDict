b19ae578b045e6f1966fe4adb46f8010ad5f080b
osfans <waxaca@163.com>
2024-11-18 10:27:48 +0800

    fix: not close dropdrow when click item

--- app/src/main/java/com/osfans/mcpdict/MultiLanguageAdapter.java
+++ app/src/main/java/com/osfans/mcpdict/MultiLanguageAdapter.java
@@ -4,12 +4,14 @@ import android.content.Context;
 import android.database.Cursor;
 import android.graphics.Color;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.util.Set;
 
 public class MultiLanguageAdapter extends LanguageAdapter {
     int mColorHighlight;
+    View.OnClickListener onClick = null;
 
     public MultiLanguageAdapter(Context context, Cursor c, boolean autoRequery) {
         super(context, c, autoRequery);
@@ -23,5 +25,26 @@ public class MultiLanguageAdapter extends LanguageAdapter {
         TextView tv = (TextView)view;
         tv.setText(language);
         tv.setBackgroundColor(set.contains(language) ? mColorHighlight : Color.TRANSPARENT);
+    }
+
+    public void setOnItemClickListener(View.OnClickListener onClick) {
+        this.onClick = onClick;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+         View v = super.getView(position, convertView, parent);
+         setCustomOnClick(v, position);
+         return v;
+    }
+
+    private void setCustomOnClick(final View view, final int position){
+        view.setTag(position);
+        view.setOnClickListener(v -> {
+            if(onClick==null)
+                return;
+            onClick.onClick(v);
+            notifyDataSetChanged();
+        });
     }
 }
