58805f73baccaf10c30d761787b377a013930080
osfans <waxaca@163.com>
2024-11-22 14:53:16 +0800

    匯纂

--- tools/tables/匯纂.py
+++ tools/tables/匯纂.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from tables._表 import 表 as _表
+import re
 
 class 表(_表):
 	full = "古音匯纂"
@@ -11,4 +12,4 @@ class 表(_表):
 	ybTrimSpace = False
 	
 	def parse(self, fs):
-		return fs[0], fs[1], "\t".join(fs[2:]).replace("▲", "\t▲")
+		return fs[0], fs[1], re.sub("｜.*?\t", "\t", "\t".join(fs[2:]).replace("▲", "\t▲"))
