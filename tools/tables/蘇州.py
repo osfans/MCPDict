83a4d4de2d22fe031041224072b7ad9a3d372c91
osfans <waxaca@163.com>
2024-12-16 16:38:18 +0800

    1216

---
+++ tools/tables/蘇州.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import re
+from tables._音典 import 表 as _表
+
+class 表(_表):
+	_files = ["苏州（记音替换版）1.1.tsv", "苏州.tsv"]
+	loosedict = dict()
+
+	def parse(self, fs):
+		if self.current_file == self._files[0]:
+			order, hz, sm, ym, sd, js = fs[:6]
+			self.loosedict[order+hz] = sm+ym+sd
+			return
+		order, hz, sm, ym, sd, js = fs[:6]
+		yb = sm + ym + sd
+		loose = self.loosedict.get(order+hz, "")
+		if loose and loose != yb.replace("*", ""):
+			yb = yb + "/" + loose
+		return hz, yb, js
+
