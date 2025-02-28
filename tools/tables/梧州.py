1b376bbf84eacd0fe0c56b4492958113f21336e0
osfans <waxaca@163.com>
2025-02-28 18:15:34 +0800

    0228

--- tools/tables/梧州.py
+++ tools/tables/梧州.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import re
-from collections import defaultdict
 from tables._表 import 表 as _表
 
 class 表(_表):
@@ -9,8 +8,9 @@ class 表(_表):
 		if "[" not in 列[0]: return
 		l = list()
 		行 = 列[0].replace(" ", "")
-		for yb, 字組 in re.findall(r"\[(.*?)\]([^[]+)", 行):
-			yb = 自.轉調類(yb)
+		for 音, 字組 in re.findall(r"\[(.*?)\]([^[]+)", 行):
+			音 = 自.轉調類(音)
+			字組 = 自.normS(字組, "（\\1）")
 			for 字, js in re.findall("(.)(（.*?）)?", 字組):
-				l.append((字, yb, js.strip("（）")))
+				l.append((字, 音, js.strip("（）")))
 		return l
