9c9d5bc1a02319e7bc6689acb6a4d6876540982d
osfans <waxaca@163.com>
2025-05-08 17:44:28 +0800

    0508

--- tools/pdf2html.py
+++ tools/pdf2html.py
@@ -1,11 +1,13 @@
-import re, sys
+import re, sys, os
 from io import StringIO
 from pdfminer.high_level import extract_text_to_fp
+from pdfminer.layout import LAParams
 
+os.system("del /f /q Im*.jpg")
+laparams=None#LAParams(char_margin=2.0, line_margin=0.5, word_margin=0.1)
 with StringIO() as out:
-    extract_text_to_fp(inf=open(sys.argv[1], "rb"), output_type="html", outfp=out, codec=None, output_dir=".")
-    out.seek(0)
-    html = out.read()
+    extract_text_to_fp(inf=open(sys.argv[1], "rb"), output_type="html", outfp=out, codec=None, output_dir=".", laparams=None)
+    html = out.getvalue()
 html = re.sub(r"(b'.*?')", lambda x:eval(x.group(1)).decode("gb18030"), html)
 html = re.sub(r'<span style="position:absolute;.*?></span>', "", html, flags=re.M|re.S)
 html = re.sub(r'<a .*?>.*?</a>', "", html, flags=re.M|re.S)
@@ -17,90 +19,7 @@ html = re.sub("(\n<br>)([^<])", "\\2", html, flags=re.M|re.S)
 html = re.sub(r'(\n<br>)(</span><span style="font-family: 方)', "\\2", html, flags=re.M|re.S)
 html = re.sub(r'(\n<br>)(</span><span style="font-family: [^"]+7px)', "\\2", html, flags=re.M|re.S)
 html = re.sub(r'(\n<br>)(</span><span style="font-family: [^"]+font-size:10px">□)', "\\2", html, flags=re.M|re.S)
-d1 = {
-    "":"1",
-    "":"3",
-    "":"5",
-    "":"7",
-    "":"ʰ",
-    "": "ʐ",
-    "": "ɕ",
-    "": "ŋ",
-    "": "∅",
-    "": "ɿ",
-    "": "ʅ",
-    "": "ə",
-    "": "ɤ",
-    "": "æ",
-    "": "ɔ",
-    "": "ʔ",
-    "": "꜔",
-    "": "˩",
-    "": "꜒꜔",
-    "": "꜔꜒",
-    "?": "ɒ",
-    "": "˙",
-}
-d2 = {
-    "":"1",
-    "":"ə",
-    "":"ɔ",
-    "":"∅",
-    "":"",
-    "":"2",
-    "":"ȵ",
-    "":"ɛ",
-    "":"ʐ",
-    "":"5",
-    "":"",
-    "":"ŋ",
-    "":"3",
-    "":"ɕ",
-    "":"ʰ",
-    "":"ɤ",
-    "":"ʅ",
-    "":"ʔ",
-    "":"ɛ̃",
-    "":"ɿ",
-    "":"7",
-    "":"ʂ",
-    "":"",
-    "":"˙",
-}
-d3 = {
-    "":"21",
-    "":"ə",
-    "":"ɔ",
-    "":"∅",
-    "":"ȵ",
-    "":"ɛ",
-    "":"ʐ",
-    "":"ŋ",
-    "":"53",
-    "":"ɕ",
-    "":"ʰ",
-    "":"ɤ",
-    "":"ʅ",
-    "":"ʔ",
-    "":"ɛ̃",
-    "":"ɿ",
-    "":"ʂ",
-    "?":"tʂ",
-    "":"",
-    "":"˙",
-    "":"22",
-    "":"æ",
-    "":"㊂",
-    "":"ʌ",
-    "":"ɭ",
-    "":"24",
-    "":"24",
-    "":"㊂",
-    "":"ɣ",
-    "":"᪶",
-    "":"㊀",
-    "":"㊁",
-}
+html = re.sub(r'(<span style="font-family: [^"]+7px">)(.*?)(</span>)', "{\\2}", html, flags=re.M|re.S).replace("}{", "").replace("\n<br>}", "}")
 d = {
     "":"ə",
     "":"ɔ",
@@ -109,7 +28,7 @@ d = {
     "":"ɛ",
     "":"ʐ",
     "":"ŋ",
-    "":"1",
+    "":"3",
     "":"ɕ",
     "":"ʰ",
     "":"ɤ",
@@ -118,7 +37,7 @@ d = {
     "":"ɛ̃",
     "":"ɿ",
     "":"ʂ",
-    "?":"tʂ",
+    "?":"ʯ",
     "":"˙",
     "":"æ",
     "":"㊂",
@@ -135,7 +54,7 @@ d = {
     "":"ɐ",
     "":"ɡ",
     "":"ɦ",
-    "":"7",
+    "":"1",
     "":"5",
     "":"",
     "":"",
@@ -144,7 +63,7 @@ d = {
     "":"",
     "":"ɠ",
     "":"○",
-    "":"6",
+    "":"2",
     "":"ɓ",
     "":"",
     "":"8",
@@ -156,17 +75,41 @@ d = {
     "":"ẽ",
     "":"7",
     "":"ɯ",
-    "":"",
+    "":"ɷ",
     "":"",
-    "":"",
-    "":"",
+    "":"˨˩",
+    "":"6",
+    "":"1",
+    "":"3",
+    "":"5",
+    "":"2",
+    "":"ã",
+    "":"ɔ̃",
+    "":"ʒ",
+    "":"ɪ̃",
+    "":"ʊ",
+    "":"",
+    "":"˨",
+    "":"",
+    "":"",
+    "":"",
+    "":"",
+    "":"",
+    "":"",
+    "":"",
+    "":"",
 }
 for i,j in d.items():
     if j:
         html = html.replace(i, j)
-html = re.sub(r'(<span style="font-family: ZIeFfH-\d; font-size:10px">[^\d□])', '\n<br>\\1', html, flags=re.M|re.S)
 
-puas = set(re.findall("[\ue000-\uefff]", html))
+puas = re.findall("[\ue000-\uefff]", html)
+puad = dict()
 for i in puas:
-    print(f'"{i}":"",')
+    if i not in puad:
+        puad[i] = 1
+    else:
+        puad[i] += 1
+for i in sorted(puad.keys(), key=lambda x: puad[x], reverse=True):
+    print(f'\t"{i}":"",')
 open(sys.argv[2], "w", encoding="U8").write(html)
