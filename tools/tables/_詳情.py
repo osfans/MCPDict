1a419cdc44acd6241eaee83978d47ff05336d02f
osfans <waxaca@163.com>
2025-09-03 11:17:44 +0800

    0903

--- tools/tables/_詳情.py
+++ tools/tables/_詳情.py
@@ -32,13 +32,13 @@ def getTones(tones):
 		i = i + 1
 		index = i
 		if not ts: continue
-		ts = str(ts).lower()
+		ts = str(ts).lower().replace("ˀ", "6").replace("ʔ", "0")
 		for j,t in enumerate(ts.split(",")):
 			if t.startswith("["):
 				index = t[1:t.index("]")]
 				t = t[t.index("]")+1:]
 			if t[0].isdigit() or t[0] == "-":
-				n = t.lstrip("012345-/")
+				n = t.lstrip("0123456-/")
 				v = t[:len(t)-len(n)]
 			else:
 				n = t
