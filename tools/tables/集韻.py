51b2ec10137ce5450be04bfb530cb6bea4e425c1
osfans <waxaca@163.com>
2026-05-14 22:23:26 +0800

    chore: 集韻

--- tools/tables/集韻.py
+++ tools/tables/集韻.py
@@ -15,13 +15,13 @@ class 表(_表):
 		if 行.startswith("### "): 自.反切 = 行.strip("# ")
 		elif 行.startswith("## "): 自.韻 = 行.strip("# ")
 		elif 行.startswith("# "): 自.卷 = 行.strip("# ")
-		elif "`" not in 行: 行 = ""
 		return 行
 	
 	def 析(自, 列):
+		if not 列[0].endswith("`"): return
 		字, 註 = 列[0].split("`")[:2]
-		音 = f"{自.卷[6]}{自.卷[4]}{自.韻[0]}。{自.反切.rstrip("切")}"
-		字 = 字.replace("=", "")
+		音 = f"{自.卷[6]}{自.卷[4]}{自.韻[0]}。{自.反切.split(" ")[0].rstrip("切")}"
+		字 = 字.replace("=", "").replace("[", "").replace("]", "")
 		l = list()
 		for i, ids in re.findall(r"(.)(\(.*?\))?", 字):
 			l.append((i, 音, ids + 註))
