46efd4aa48547dff901bae151fd32252df9be474
osfans <waxaca@163.com>
2026-08-15 11:40:39 +0800

    feat: 嘉禾塘村 嘉禾石橋 平垻 新無錫 永福羅田 泰順司前 興仁 莘縣張魯 華寧

--- tools/tables/_跳跳老鼠.py
+++ tools/tables/_跳跳老鼠.py
@@ -19,7 +19,7 @@ class 表(_表):
 			if len(列) <= 列序[0]: return
 			組 = 列[列序[0]]
 			if len(列序) == 1:
-				if 名 in ("英德魚灣",):
+				if 名 in ("英德魚灣","莘縣張魯"):
 					上標 = "⁰¹²³⁴⁵⁶⁷⁸⁹"
 					組 = re.sub(r"([⁰¹²³⁴⁵⁶⁷⁸⁹]+)", lambda x:"".join([str(上標.index(i)) for i in x.group(1)]), 組)
 				if g := re.findall("^.*?\\d+", 組):
